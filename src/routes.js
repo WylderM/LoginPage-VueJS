4f1998c2b3f88f474246199923042c2f779e55aa
WylderM <wyldermenezes@gmail.com>
2020-09-22 21:59:05 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const Login = { template: '<div>Login</div>' };
+const Home = { template: '<div>Home</div>' };
+
+const routes = [
+	{ path: '/login', component: Login },
+	{ path: '/home', component: Home },
+];
